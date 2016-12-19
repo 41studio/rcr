46ce270910af8c3dfe54981c9a0666f97d8874ae
Mada Aryakusumah <mada@41studio.com>
2016-12-19 16:34:48 +0700

    setup versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  apipie
   devise_for :users
-  post 'auth_user' => 'api/api_authentication#authenticate_user'
+  post 'api/v1/auth_user' => 'api/v1/api_authentication#authenticate_user'
   
   namespace :api do
-    resources :areas
-    resources :appraisals
-    resources :indicators, only: :index
+    namespace :v1 do 
+      resources :areas
+      resources :appraisals
+      resources :indicators, only: :index
+    end
   end
 
   resources :companies
   resources :appraisals
-  resources :indicators
-  resources :areas
   resources :items
 end
