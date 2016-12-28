047c75ece51e32aebf0f3edcbaddc46a250669e8
Mada Aryakusumah <mada@41studio.com>
2016-12-28 09:03:34 +0700

    add role for json login API

--- app/controllers/api/v1/api_authentication_controller.rb
+++ app/controllers/api/v1/api_authentication_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::ApiAuthenticationController < Api::V1::ApiController
     return nil unless user and user.id
     {
       auth_token: JsonWebToken.encode({user_id: user.id}),
-      user: {id: user.id, email: user.email}
+      user: {id: user.id, email: user.email, role: user.role.try(:name)}
     }
   end
 end
