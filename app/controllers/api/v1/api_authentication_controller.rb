ab8b72bc02cd8855925993cef9015d015503ad0e
Ervina Anggraeni <ervina@41studio.com>
2016-12-28 16:39:15 +0700

    Add apipie doc for auth token and indicators

--- app/controllers/api/v1/api_authentication_controller.rb
+++ app/controllers/api/v1/api_authentication_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::ApiAuthenticationController < Api::V1::ApiController
+  
+  # POST /api/v1/auth_user
+  api :POST, "/auth_user", "Authentication user"
+  param :email, String, required: true, desc: "Email of user"
+  param :password, String, required: true, desc: "Password of user"
+  formats ['json']
   def authenticate_user
     user = User.find_for_database_authentication(email: params[:email])
     if user.valid_password?(params[:password])
