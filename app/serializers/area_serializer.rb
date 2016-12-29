cb717d0a60481d459cb18ee9303aad7ac264a010
Ervina Anggraeni <ervina@41studio.com>
2016-12-29 13:22:43 +0700

    add indicator code to appraisal

--- app/serializers/area_serializer.rb
+++ app/serializers/area_serializer.rb
@@ -11,9 +11,10 @@ class AreaSerializer < ActiveModel::Serializer
         appraisal_items = []
         item_area_member = {id: item_time.id, time: item_time.time}
 
-        item_time.appraisals.by_day(object.search_date).each do |appraisal|
+        item_time.appraisals.includes(:indicator).by_day(object.search_date).each do |appraisal|
           appraisal_items << appraisal.attributes.delete_if {|key| ['created_at', 'updated_at'].include? key }.merge(
-            {indicator: appraisal.indicator_description, checked_at: appraisal.created_at.strftime("%Y-%m-%d"), reviewed_at: appraisal.updated_at.strftime("%Y-%m-%d")})
+            { indicator_code: appraisal.indicator.code, indicator_description: appraisal.indicator.description, 
+              checked_at: appraisal.created_at.strftime("%Y-%m-%d"), reviewed_at: appraisal.updated_at.strftime("%Y-%m-%d") })
         end
 
         item_area_member[:appraisals] = appraisal_items
