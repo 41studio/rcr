f036b3c89891733b851d764d6dd9a91444cd5f24
Ervina Anggraeni <ervina@41studio.com>
2017-01-09 16:17:07 +0700

    fix

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,45 +1,45 @@
 class Api::V1::ApiController < ActionController::API
-  include Rails::Pagination
-  include ActionController::Serialization
   attr_reader :current_user
 
   protected
-  def authenticate_request!
-    unless user_id_in_token?
-      render json: { error: 'Not Authenticated' }, status: :unauthorized
-      return
-    end
-    @current_user = User.find(auth_token[:user_id])
-  rescue JWT::VerificationError, JWT::DecodeError
-    render json: { error: 'Not Authenticated' }, status: :unauthorized
-  end
 
-  def authenticate_owner_or_manager!
-    unless @current_user.is_owner? || @current_user.is_manager?
-      render json: { error: 'Not authorized, for owner and manager role only' }, status: :unauthorized
+    def authenticate_request!
+      unless user_id_in_token?
+        render json: { error: 'Not Authenticated' }, status: :unauthorized
+        return
+      end
+      @current_user = User.find(auth_token[:user_id])
+    rescue JWT::VerificationError, JWT::DecodeError
+      render json: { error: 'Not Authenticated' }, status: :unauthorized
     end
-  end
+
+    def authenticate_owner_or_manager!
+      unless @current_user.is_owner? || @current_user.is_manager?
+        render json: { error: 'Not authorized, for owner and manager role only' }, status: :unauthorized
+      end
+    end
 
   private
-  def http_token
-    @http_token ||= if request.headers['Authorization'].present?
-      request.headers['Authorization'].split(' ').last
+
+    def http_token
+      @http_token ||= if request.headers['Authorization'].present?
+        request.headers['Authorization'].split(' ').last
+      end
     end
-  end
 
-  def auth_token
-    @auth_token ||= JsonWebToken.decode(http_token)
-  end
+    def auth_token
+      @auth_token ||= JsonWebToken.decode(http_token)
+    end
 
-  def user_id_in_token?
-    http_token && auth_token && auth_token[:user_id].to_i
-  end
+    def user_id_in_token?
+      http_token && auth_token && auth_token[:user_id].to_i
+    end
 
-  def payload(user)
-    return nil unless user and user.id
-    {
-      auth_token: JsonWebToken.encode({user_id: user.id}),
-      user: { id: user.id, email: user.email, role: user.role.try(:name), name: user.name }
-    }
-  end
+    def payload(user)
+      return nil unless user and user.id
+      {
+        auth_token: JsonWebToken.encode({user_id: user.id}),
+        user: { id: user.id, email: user.email, role: user.role.try(:name), name: user.name }
+      }
+    end
 end
