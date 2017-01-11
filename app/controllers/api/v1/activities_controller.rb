ac57226a37fcaf9649f7ab4bc537c4b353fa0271
Ervina Anggraeni <ervina@41studio.com>
2017-01-11 17:31:57 +0700

    Add validation to check timezone

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ActivitiesController < Api::V1::ApiController
   before_action :authenticate_request!
+  before_action :check_available_timezone
 
   # GET /api/v1/activities
   api :GET, "/activities", "Get list of activities based on company"
@@ -12,6 +13,18 @@ class Api::V1::ActivitiesController < Api::V1::ApiController
     @activities = Activity.joins_with_trackable(@current_user, params[:date]).page(params[:page]).per(10)
 
     render json: @activities, meta: pagination_dict(@activities), context: { timezone: params[:timezone] }, each_serailizer: ActivitySerializer
+  end
+
+  private
+
+  def check_available_timezone
+    timezones = ActiveSupport::TimeZone::MAPPING.values
+    
+    if params[:timezone].present?
+      unless timezones.include?(params[:timezone])
+        render json: { error: "Invalid timezone" }, status: :unprocessable_entity
+      end
+    end
   end
 
 end
